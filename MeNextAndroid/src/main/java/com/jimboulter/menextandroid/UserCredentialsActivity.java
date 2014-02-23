d72ec9bcdaa6fc0cdbf543cfbfd3df88b02bb513
jboulter11 <jboulter11@gmail.com>
2014-02-22 19:32:34 -0500

    First Commit
    Other project corrupted, moved to new project.  Gradle is weird.

---
+++ MeNextAndroid/src/main/java/com/jimboulter/menextandroid/UserCredentialsActivity.java
@@ -0,0 +1,56 @@
+package com.jimboulter.menextandroid;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+/**
+ * Created by Jim Boulter on 2/15/14.
+ */
+public class UserCredentialsActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        SharedPreferences sharedPref = getPreferences(Context.MODE_PRIVATE);
+        if(!sharedPref.getString(getString(R.string.username_setting),
+                getString(R.string.default_setting)).equals(getString(R.string.default_setting)))
+        {
+            EditText usernameText = (EditText) findViewById(R.id.usernameText);
+            usernameText.setText((CharSequence)
+                    sharedPref.getString(getString(R.string.username_setting),
+                            getString(R.string.default_setting)), TextView.BufferType.EDITABLE);
+        }
+        else
+        {
+            Toast.makeText(this, getString(R.string.ask_for_cred), Toast.LENGTH_LONG).show();
+        }
+
+        setContentView(R.layout.user_credentials_activity);//display new view
+
+        Button loginButton = (Button)findViewById(R.id.loginButton);
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                SharedPreferences sharedPref = getPreferences(Context.MODE_PRIVATE);
+                SharedPreferences.Editor editor = sharedPref.edit();
+                editor.putString(getString(R.string.username_setting),
+                        getString(R.id.usernameText));
+                editor.putString(getString(R.string.password_setting),
+                        getString(R.id.passwordText));
+                editor.commit();
+                finish();
+            }
+        });
+    }
+
+
+}
